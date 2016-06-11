ae27e2a7117542a26dece64ada36b3df9bd8033d
evdokimoveu <evdokimoveu@gmail.com>
2016-06-11 19:21:02 +0300

    Update TMDBConfig.java

--- app/src/main/java/com/evdokimoveu/testapplication/TMDBConfig.java
+++ app/src/main/java/com/evdokimoveu/testapplication/TMDBConfig.java
@@ -3,7 +3,7 @@ package com.evdokimoveu.testapplication;
 
 public class TMDBConfig {
     public static final String URL = "https://api.themoviedb.org/3/";
-    public static final String KEY = "450f3b611bef52622b0453f85fb11d52";
+    public static final String KEY = "KEY_KEY";
     public static final String IMG_URL = "https://image.tmdb.org/t/p/";
     public static final String YOUTUBE_PATH = "https://www.youtube.com/watch?v=";
     public static final String MOVE = "movie/";
